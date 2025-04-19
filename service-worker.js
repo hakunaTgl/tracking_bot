38eeba9d9a2c430a7d86d30911376f28bb2cacc9
Hakuna <98fenwickt@gmail.com>
2025-04-19 00:04:09 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,23 @@
+<xaiArtifact artifact_id="10e73b61-63c7-4a98-aa82-4bb4b61a32d1" artifact_version_id="c0718dc3-0f21-4a32-ac18-2dd9edec5ca7" title="service-worker.js" contentType="application/javascript">
 self.addEventListener('install', event => {
-event.waitUntil(
-caches.open('bot-hub-cache-v1').then(cache => {
-return cache.addAll([
-'/',
-'/index.html',
-'/manifest.json'
-]);
-})
-);
+    event.waitUntil(
+        caches.open('bot-hub-cache-v2').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/icon.png'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request).catch(() => {
-return caches.match('/index.html');
-});
-})
-);
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).catch(() => {
+                return caches.match('/index.html');
+            });
+        })
+    );
 });
